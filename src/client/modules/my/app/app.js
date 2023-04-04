58ae674060c0948982bf8fc56041a707e27f2640
Naveen Mutha <naveen.mutha@eleks.com>
2023-04-04 09:06:49 -0500

    server

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,3 +1,21 @@
 import { LightningElement } from 'lwc';
 
-export default class App extends LightningElement {}
+export default class App extends LightningElement {
+    connectedCallback() {
+         fetch('/oauth2/auth',
+         {
+             // Request type
+             method:"GET",
+             headers:{
+                 "mode": "no-cors",
+                 "Access-Control-Allow-Origin": '*',
+                 "Accept": "application/json"
+             }
+             }).then(response => {
+                console.log(response);
+                
+            });
+        }
+
+
+}
--- src/server/api.js
+++ src/server/api.js
@@ -7,9 +7,14 @@ const app = express();
 app.use(helmet());
 app.use(compression());
 
-const HOST = process.env.API_HOST || 'localhost';
-const PORT = process.env.API_PORT || 3002;
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 3002;
+const DIST_DIR = './dist';
+app.use(express.static(DIST_DIR));
 
+app.use('/^(?!\/api).+/', (req, res) => {
+    res.sendFile(path.resolve(DIST_DIR, 'index.html'));
+});
 app.get('/api/v1/endpoint', (req, res) => {
     res.json({ success: true });
 });
@@ -19,3 +24,35 @@ app.listen(PORT, () =>
         `✅  API Server started: http://${HOST}:${PORT}/api/v1/endpoint`
     )
 );
+
+
+const jsforce = require('jsforce');
+const oauth2 = new jsforce.OAuth2({
+    // you can change loginUrl to connect to sandbox or prerelease env.
+     loginUrl : 'https://login.salesforce.com',
+    clientId : '3MVG9ux34Ig8G5eo69lXF2jdlWctP3DHEpmXsEbB13BJ5WzXfzFF1XZBBxX44fKN1N4G8jegw5yI0XURUSfT6',
+    clientSecret : 'ED5B3198DB229E3EC1CFF0387C861D82046406D36740F873FC9D927047A5FF0A',
+    redirectUri : 'https://eleks.herokuapp.com/oauth2/callback'
+
+});
+app.get('/oauth2/auth', function(req, res) {
+    console.log('new update')
+    res.redirect(oauth2.getAuthorizationUrl({ scope : 'api id web' }));
+  });
+
+  app.get('/oauth2/callback', function(req, res) {
+    const conn = new jsforce.Connection({ oauth2 : oauth2 });
+    const code = req.param('code');
+    conn.authorize(code, function(err, userInfo) {
+      if (err) { return console.error(err); }
+      // Now you can get the access token, refresh token, and instance URL information.
+      // Save them to establish connection next time.
+      console.log(conn.accessToken);
+      console.log(conn.refreshToken);
+      console.log(conn.instanceUrl);
+      console.log("User ID: " + userInfo.id);
+      console.log("Org ID: " + userInfo.organizationId);
+      // ...
+      res.send('success'); // or your desired response
+    });
+  });
